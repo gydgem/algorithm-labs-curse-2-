73a22f88982ce55324ea3a4e81bd2f54d9580e84
gydgem <glebvyskvarko630@gmail.com>
2024-09-12 00:00:03 +0300

    add edge and vertex

---
+++ GrapsLab1/graffComponents/Edge.cpp
@@ -0,0 +1,8 @@
+//
+// Created by gydgem on 9/11/2024.
+//
+
+#include "Edge.h"
+
+namespace hgem {
+} // hgem
---
+++ GrapsLab1/graffComponents/Vertex.cpp
@@ -0,0 +1,8 @@
+//
+// Created by gydgem on 9/11/2024.
+//
+
+#include "Vertex.h"
+
+namespace hgem {
+} // hgem
